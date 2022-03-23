f0ceaeb1fa69e14da11a9c31970ea10ab94fdb51
Gildéric DERUETTE <gilderic.deruette@kleegroup.com>
2022-03-23 20:18:18 +0100

    Ajout test clean empties

--- projects/ngx-farmulor/src/lib/ngx-farmulor.service.spec.ts
+++ projects/ngx-farmulor/src/lib/ngx-farmulor.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
-import { NgxFarmulorService } from './ngx-farmulor.service';
+import { cleanEmpties, NgxFarmulorService } from './ngx-farmulor.service';
 
 describe('NgxFarmulorService', () => {
   let service: NgxFarmulorService;
@@ -12,5 +12,11 @@ describe('NgxFarmulorService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('CleanEmpties', () => {
+    const toTest: Object = { a: null, b: "toto", c: { c1: undefined, c2: [], c3: "C3" } };
+    cleanEmpties(toTest)
+    expect(toTest).toEqual({ b: "toto", c: { c3: "C3" } })
   });
 });
